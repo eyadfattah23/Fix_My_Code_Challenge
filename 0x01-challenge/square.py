2c1a0e504bfa584575f64405a246211850ee6204
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-12-14 14:04:50 +0200

    remove new spaces

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 class square():
-
+    
     width = 0
     height = 0
 
+    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -18,7 +19,6 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
-
 
 if __name__ == "__main__":
 
